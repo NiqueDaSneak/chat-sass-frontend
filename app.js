1b5501b7f4c25d7be529d1cba7b2bcfc5058f264
Dom Clemmer <thisisdom4@gmail.com>
2017-06-20 11:03:11 -0400

    use html for views

--- app.js
+++ app.js
@@ -2,26 +2,27 @@
 
 // NPM PACKAGES
 const express = require('express')
+var path = require("path")
 
 // EXPRESS SETUP
 const app = express()
 app.use(express.static('public'))
-app.set('views', __dirname + '/views')
-app.set('view engine', 'pug')
+app.use(express.static(__dirname + 'view'))
+// app.set('view engine', 'pug')
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.render('index')
+  res.sendFile(path.join(__dirname + '/views/index.html'))
 })
 
 // SET UP SERVER ENVIRONMENT
-var port = process.env.PORT || 3000;
-server.listen(port, function(){
-    console.log('Server running on port ' + port);
-});
+var port = process.env.PORT || 3000
+app.listen(port, function(){
+    console.log('Server running on port ' + port)
+})
 
 app.on('error', function(){
-    console.log(error);
-});
+    console.log(error)
+})
 
-module.exports = app;
+module.exports = app
