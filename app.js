70afa9cb063cef8541ccbad83342e1aad1e46609
Dom Clemmer <thisisdom4@gmail.com>
2017-06-20 16:06:43 -0400

    set post routes for forms, and install body-parser

--- app.js
+++ app.js
@@ -2,17 +2,32 @@
 
 // NPM PACKAGES
 const express = require('express')
-var path = require("path")
+const path = require("path")
+const bodyParser = require('body-parser')
 
 // EXPRESS SETUP
 const app = express()
 app.use(express.static('public'))
 app.use(express.static(__dirname + 'view'))
 // app.set('view engine', 'pug')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // ROUTES
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'))
+})
+
+app.get('/auth', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/auth.html'))
+})
+
+app.post('/login', (req, res) => {
+  console.log(req.body)
+})
+
+app.post('/signup', (req, res) => {
+  console.log(req.body)
 })
 
 // SET UP SERVER ENVIRONMENT
