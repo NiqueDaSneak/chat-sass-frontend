50a56401684e527af3c642bc497a2d6a9f8d29d7
Dom Clemmer <thisisdom4@gmail.com>
2017-06-24 14:26:41 -0400

    get calendar on the screen!!!

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,9 +1,18 @@
 $(document).ready(() => {
 
+  // GRAB ORG NAME FOR URL TO GET DATA FROM SERVER
   var pathname = window.location.pathname.split('/')
   var org = pathname[pathname.length - 1]
   console.log(org)
 
+  // CALENDAR SETUP
+  $('#calendar').fullCalendar({
+        // put your options and callbacks here
+  })
+
+
+  // SOCKET CONNECTION AND DATA TRANSFER
   var socket = io.connect();
+
 
 })
