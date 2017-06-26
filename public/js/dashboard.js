162a9d037e0ed3c23f399551e08f3d6600dc023c
Dom Clemmer <thisisdom4@gmail.com>
2017-06-25 21:22:48 -0400

    working main calendar controls

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,15 +1,26 @@
 $(document).ready(() => {
 
+  // SET DATE FOR TOGGLE UI ELEMENT
+  $('.month').text(moment().format("MMMM"))
+
   // GRAB ORG NAME FOR URL TO GET DATA FROM SERVER
   var pathname = window.location.pathname.split('/')
   var org = pathname[pathname.length - 1]
   console.log(org)
 
-  // UI
+  // UI & INTERACTIONS
 
   $('.toggle').click(() => {
     $('footer').toggleClass('active')
     $('#toggle-calendar').toggleClass('bottom')
+  })
+
+  $('.left').click(() => {
+    $('#main-calendar').fullCalendar('prev')
+  })
+
+  $('.right').click(() => {
+    $('#main-calendar').fullCalendar('next')
   })
 
   // CALENDAR SETUP
@@ -24,13 +35,7 @@ $(document).ready(() => {
     }
   })
 
-  $('.left').click(() => {
-    $('#main-calendar').fullCalendar('prev')
-  })
 
-  $('.right').click(() => {
-    $('#main-calendar').fullCalendar('next')
-  })
 
   // SOCKET CONNECTION AND DATA TRANSFER
   var socket = io.connect();
