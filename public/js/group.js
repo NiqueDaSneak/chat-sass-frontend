3ef87d698e923671652f863444ecd10c893a9ed2
Dom Clemmer <thisisdom4@gmail.com>
2017-07-24 12:26:27 -0400

    add interactions and data requests

--- public/js/group.js
+++ public/js/group.js
@@ -1,14 +1,61 @@
 $(document).ready(() => {
 
-  $('.list div').click(() => {
-    console.log('clicked')
-    $('.list').toggleClass('rotate')
-    $('.new').toggleClass('rotate')
+  // find org name from URL
+  var pathname = window.location.pathname.split('/')
+  var org = pathname[pathname.length - 1]
+
+  // socket connection
+  var socket = io.connect()
+
+  // UI & INTERACTIONS
+  $('.showGroups').click(() => {
+    socket.emit('getList', {org: org})
+    $('.groups').toggleClass('hide')
+  })
+
+  $('.list .addGroup').click(() => {
+    socket.emit('requestMembers', {data: org})
+    $('.list').toggleClass('hide')
+    $('.new').toggleClass('hide')
   })
 
   $('.new button').click(() => {
-    $('.list').toggleClass('rotate')
-    $('.new').toggleClass('rotate')
+    var arr = []
+    for (var i = 0; i < $('.names').children().length; i++) {
+      if ($($('.names').children()[i]).hasClass('selected')) {
+        arr.push($($('.names').children()[i]).data('fbid'))
+      }
+    }
+    socket.emit('createGroup', {groupMembers: arr, groupName: $('.groupName').val(), org: org})
+    socket.emit('getList', {org: org})
+    $('.new').toggleClass('hide')
+    $('.list').toggleClass('hide')
+    $('.names').empty()
+    $('.groupName').val("")
+  })
+
+  $('.groups .header img').click(() => {
+    $('.groups').toggleClass('hide')
+  })
+
+  $('.submenu').click(() => {
+    $('footer').toggleClass('sub')
+    $('.submenu').toggleClass('inactive')
+  })
+
+  $('.new').click((event) => {
+    $(event.target).parent().toggleClass('selected')
+  })
+
+  socket.on('addUser', (data) => {
+    $('.new .names').prepend("<div data-fbid=" + data.data.fbID + "><img src='" + data.data.photo + "' alt='profile photo'><p>" + data.data.fullName + "</p></div>")
+  })
+
+  socket.on('showList', (data) => {
+    $('.listNames').empty()
+    for (var i = 0; i < data.data.length; i++) {
+      $('.list .listNames').append("<p>" + data.data[i].groupName + "</p>")
+    }
   })
 
 })
--- public/js/submenu.js
+++ public/js/submenu.js
@@ -1,8 +1,5 @@
 $(document).ready(() => {
 
-  // socket connection
-  var socket = io.connect()
-
   $('.hamburger').click(() => {
     $('footer').toggleClass('sub')
     $('.submenu').toggleClass('inactive')
