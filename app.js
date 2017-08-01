ccc72e578306a99830142ef47d3d40384cf920e5
Dom Clemmer <thisisdom4@gmail.com>
2017-08-01 13:45:18 -0400

    add new scope for ad management

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ passport.use(new FacebookStrategy({
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
 //     /auth/facebook/callback
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: [ 'email', 'pages_show_list', 'manage_pages'] }))
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: [ 'ads_management', 'email', 'pages_show_list', 'manage_pages'] }))
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
