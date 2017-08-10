3a17f70dd8cb4b442dea660eb8ef4d1397f4506c
Dom Clemmer <thisisdom4@gmail.com>
2017-08-10 14:04:05 -0400

    change url

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ app.use(passport.session())
 passport.use(new FacebookStrategy({
     clientID: '372903006444693',
     clientSecret: 'e0cf0b310d6931c9140969a115efefa9',
-    callbackURL: "http://localhost:3000/auth/check-pages",
-    // callbackURL: "http://chat-sass-frontend.herokuapp.com/auth/check-pages",
+    // callbackURL: "http://localhost:3000/auth/check-pages",
+    callbackURL: "http://chat-sass-frontend.herokuapp.com/auth/check-pages",
     profileFields: ['id', 'emails', 'name']
   },
   function(accessToken, refreshToken, profile, done) {
