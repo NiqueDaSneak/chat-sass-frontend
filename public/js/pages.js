96d847931b5e435f2e7232c6b4897e6e0ad4d2f5
Dom Clemmer <thisisdom4@gmail.com>
2017-08-16 14:05:35 -0400

    style page selection view

--- public/js/pages.js
+++ public/js/pages.js
@@ -8,7 +8,6 @@ $(document).ready(() => {
   socket.emit('requestPages')
 
   socket.on('addPages', (data) => {
-    console.log(data.page)
     $('.pageList').append("<a href='/save-page?access_token=" + data.page.access_token + "&pageid=" + data.page.id + "&userid=" + ID + "&org=" + data.page.name + "'>" + data.page.name + "</a>")
   })
 
