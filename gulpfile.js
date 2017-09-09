fd03720c8bec2295243fcee28fdb89f44868ddc7
Dom Clemmer <thisisdom4@gmail.com>
2017-09-09 18:30:37 -0400

    add files for js compliling

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,18 @@ const sass = require('gulp-sass')
 const gutil = require('gulp-util')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+// var jsFiles = 'public/js/*.js',
+//     jsDest = 'public/js'
+//
+// gulp.task('scripts', function() {
+//     return gulp.src(jsFiles)
+//         .pipe(concat('frontend.js'))
+//         .pipe(gulp.dest(jsDest));
+// });
 
 gulp.task('sass', function(){
   return gulp.src('./public/stylesheets/sass/*.sass')
@@ -12,10 +24,11 @@ gulp.task('sass', function(){
         .pipe(postcss([ autoprefixer() ]))
         .on('error', gutil.log)
         .pipe(gulp.dest('./public/stylesheets/css'))
-});
+})
 
 gulp.task('watch', function(){
   gulp.watch('public/stylesheets/sass/*.sass', ['sass'])
-});
+  // gulp.watch('public/js/*.js', ['scripts'])
+})
 
 gulp.task('default', ['watch', 'sass'])
