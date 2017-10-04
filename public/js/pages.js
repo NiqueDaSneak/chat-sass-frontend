af17ead76c3ba3c31373d2a4e61c4360c0e0f7de
Dom Clemmer <thisisdom4@gmail.com>
2017-10-04 14:42:11 -0400

    add correct content to page contexually depending on page content

--- public/js/pages.js
+++ public/js/pages.js
@@ -11,12 +11,13 @@ $(document).ready(() => {
   // })
   socket.emit('requestPages', {userID: userID, userAccessToken: userAccessToken})
 
-  // socket.on('addPages', (data) => {
-  //   $('.pageList').append("<a href='/save-page?access_token=" + data.page.access_token + "&pageid=" + data.page.id + "&userid=" + userID + "&org=" + data.page.name + "'>" + data.page.name + "</a>")
-  // })
+  socket.on('addPages', (data) => {
+    $('.pageList').append("<a href='/save-page?access_token=" + data.page.access_token + "&pageid=" + data.page.id + "&userid=" + userID + "&org=" + data.page.name + "'>" + data.page.name + "</a>")
+  })
 
   socket.on('noPages', () => {
-
+    $('.pageList').append('<p>Looks like you are not an admin for any Facebook Pages. Follow the link below so you can create one:</p>')
+    $('.pageList').append("<a class='no-pages-link' href='#'>Go To Facebook</a>")
   })
 
 })
