4c35a0aa928168269ba76608ae031c5be19a4ea6
Dom Clemmer <thisisdom4@gmail.com>
2018-01-05 16:40:27 -0500

    logic for full swipe capabilities

--- public/js/commerce.js
+++ public/js/commerce.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
   // start loop of email icons
   setTimeout(() => {
     $('.email-icon').remove()
-  }, 2500)
+  }, 3500)
 
   // slide messenger icon in
   setTimeout(() => {
@@ -25,134 +25,214 @@ $(document).ready(() => {
   //   // $('.subheads').append('<p>Transform Messenger into an interactive, all-in-one e-commerce tool to maximize engagement, foster relationships and sell more.</p>')
   // }, 4000)
 
-  var landingCount = 1
+  var messengerIcon = true
 
   $('.landing-nav').click((event) => {
+    if (messengerIcon === true) {
+      $('.initial-hide').css('display', 'initial')
+      console.log('...remove messenger icon')
 
-    function slideOut() {
-      return new Promise(function(resolve, reject) {
-        $('.imgs img.active').addClass('inactive')
-        $('.alert').css('left', '-77.5vw')
-        resolve()
-      })
+      $('.subheads').css('opacity', '0')
+      messengerIcon = false
+      console.log(messengerIcon)
+      $('.landing-nav span:last-of-type').addClass('active')
+      $('.landing-nav span:first-of-type').removeClass('active')
+      $('.down-chevron').css('opacity', '1')
+      $('.alert').css('left', '-77.5vw')
+      $('.imgs img.active').toggleClass('active')
+
+      setTimeout(() => {
+        $('.imgs img:first-of-type').css('display', 'none')
+      }, 500)
+
+      $('.imgs img:last-of-type').css('display', 'initial')
+      setTimeout(() => {
+        $('.imgs img:last-of-type').toggleClass('active')
+      }, 200)
+
+    } else {
+      messengerIcon = true
+      console.log('...add messenger icon')
+      $('.subheads').css('opacity', '1')
+      $('.landing-nav span:last-of-type').removeClass('active')
+      $('.landing-nav span:first-of-type').addClass('active')
+
+      $('.imgs img.active').toggleClass('active')
+      setTimeout(() => {
+        $('.imgs img:last-of-type').css('display', 'none')
+      }, 500)
+
+      $('.imgs img:first-of-type').css('display', 'initial')
+      setTimeout(() => {
+        $('.imgs img:first-of-type').toggleClass('active')
+      }, 200)
     }
 
-    // if ($(event.target).hasClass('back')) {
-    //   if (landingCount === 1) {
-    //     $('.landing-nav span:nth-of-type(' + landingCount + ')').removeClass('active')
-    //     landingCount = 2
-    //     $('.landing-nav span:last-of-type').addClass('active')
-    //   } else {
-    //     $('.landing-nav span:nth-of-type(' + landingCount + ')').removeClass('active')
-    //     landingCount--
-    //     $('.landing-nav span:nth-of-type(' + landingCount + ')').addClass('active')
-    //   }
-    // }
+  })
 
-    if ($(event.target).hasClass('fwd')) {
+  // $('.landing-nav').click((event) => {
+  //
+  //   function slideOut() {
+  //     return new Promise(function(resolve, reject) {
+  //       $('.imgs img.active').addClass('inactive')
+  //       $('.alert').css('left', '-77.5vw')
+  //       resolve()
+  //     })
+  //   }
+  //
+  //   if ($(event.target).hasClass('fwd')) {
+  //
+  //     if (landingCount === 1) {
+  //       $('.landing-nav span:first-of-type').removeClass('active')
+  //       $('.subheads').css('opacity', '0')
+  //
+  //       slideOut().then(() => {
+  //         setTimeout(() => {
+  //           $('.imgs img:first-of-type').css('display', 'none')
+  //           $('.imgs img:first-of-type').removeClass()
+  //         }, 1000)
+  //       })
+  //
+  //       landingCount++
+  //       $('.landing-nav span:nth-of-type(' + landingCount + ')').addClass('active')
+  //       $('.headers span').remove()
+  //       $('.imgs img:nth-of-type(' + landingCount + ')').css('display', 'initial')
+  //       setTimeout(() => {
+  //         $('.imgs img:nth-of-type(' + landingCount + ')').addClass('active')
+  //       }, 200)
+  //       $('.fwd').remove()
+  //       $('.back').remove()
+  //       $('.down-chevron').css('opacity', '1')
+  //       $('.initial-hide').css('display', 'initial')
+  //     }
+  //
+  //   }
+  // })
 
-      if (landingCount === 1) {
-        $('.landing-nav span:first-of-type').removeClass('active')
-        $('.subheads').css('opacity', '0')
+  let touchstartX = 0;
+  let touchstartY = 0;
+  let touchendX = 0;
+  let touchendY = 0;
 
-        slideOut().then(() => {
-          setTimeout(() => {
-            $('.imgs img:first-of-type').css('display', 'none')
-            $('.imgs img:first-of-type').removeClass()
-          }, 1000)
-        })
+  const gestureZone = document.getElementById('gestureZone');
 
-        landingCount++
-        $('.landing-nav span:nth-of-type(' + landingCount + ')').addClass('active')
-        $('.headers span').remove()
-        $('.imgs img:nth-of-type(' + landingCount + ')').css('display', 'initial')
-        setTimeout(() => {
-          $('.imgs img:nth-of-type(' + landingCount + ')').addClass('active')
-        }, 200)
-        $('.fwd').remove()
-        $('.back').remove()
-        $('.down-chevron').css('opacity', '1')
-        $('.initial-hide').css('display', 'initial')
+  gestureZone.addEventListener('touchstart', function(event) {
+      touchstartX = event.changedTouches[0].screenX;
+      touchstartY = event.changedTouches[0].screenY;
+  }, false);
+
+  gestureZone.addEventListener('touchend', function(event) {
+      touchendX = event.changedTouches[0].screenX;
+      touchendY = event.changedTouches[0].screenY;
+      handleGesture();
+  }, false);
+
+  function slideOut() {
+    return new Promise(function(resolve, reject) {
+      $('.imgs img.active').addClass('inactive')
+      $('.alert').css('left', '-77.5vw')
+      resolve()
+    })
+  }
+
+  function handleGesture() {
+      if (touchendX <= touchstartX) {
+          console.log('Swiped left')
+
+          if (messengerIcon === true) {
+            $('.initial-hide').css('display', 'initial')
+            console.log('...remove messenger icon')
+
+            $('.subheads').css('opacity', '0')
+            messengerIcon = false
+            console.log(messengerIcon)
+            $('.landing-nav span:last-of-type').addClass('active')
+            $('.landing-nav span:first-of-type').removeClass('active')
+            $('.down-chevron').css('opacity', '1')
+            $('.alert').css('left', '-77.5vw')
+            $('.imgs img.active').toggleClass('active')
+
+            setTimeout(() => {
+              $('.imgs img:first-of-type').css('display', 'none')
+            }, 500)
+
+            $('.imgs img:last-of-type').css('display', 'initial')
+            setTimeout(() => {
+              $('.imgs img:last-of-type').toggleClass('active')
+            }, 200)
+          } else {
+            messengerIcon = true
+            console.log('...add messenger icon')
+            $('.subheads').css('opacity', '1')
+            $('.landing-nav span:last-of-type').removeClass('active')
+            $('.landing-nav span:first-of-type').addClass('active')
+
+            $('.imgs img.active').toggleClass('active')
+            setTimeout(() => {
+              $('.imgs img:last-of-type').css('display', 'none')
+            }, 500)
+
+            $('.imgs img:first-of-type').css('display', 'initial')
+            setTimeout(() => {
+              $('.imgs img:first-of-type').toggleClass('active')
+            }, 200)
+          }
+
       }
 
-    }
+      if (touchendX >= touchstartX) {
+          console.log('Swiped right');
+          if (messengerIcon === true) {
+            $('.initial-hide').css('display', 'initial')
+            console.log('...remove messenger icon')
 
-    // if ($(event.target).hasClass('fwd')) {
-    //
-    //   function slideOut() {
-    //     return new Promise(function(resolve, reject) {
-    //       $('.imgs img.active').addClass('inactive')
-    //       $('.alert').css('left', '-77.5vw')
-    //       resolve()
-    //     })
-    //   }
-    //
-    //   if (landingCount === 1) {
-    //     $('.landing-nav span:first-of-type').removeClass('active')
-    //     $('.subheads').css('opacity', '0')
-    //
-    //     slideOut().then(() => {
-    //       setTimeout(() => {
-    //         $('.imgs img:first-of-type').css('display', 'none')
-    //         $('.imgs img:first-of-type').removeClass()
-    //       }, 1000)
-    //     })
-    //
-    //     landingCount++
-    //     // add active class to .subheads p:nth-of-type
-    //     $('.landing-nav span:nth-of-type(' + landingCount + ')').addClass('active')
-    //     setTimeout(() => {
-    //       $('.imgs img:nth-of-type(' + landingCount + ')').css('display', 'initial')
-    //     }, 100)
-    //     setTimeout(() => {
-    //       $('.imgs img:nth-of-type(' + landingCount + ')').addClass('active')
-    //     }, 200)
-    //   // } else if (landingCount === 2) {
-    //   //   $('.landing-nav span:nth-of-type(' + landingCount + ')').removeClass('active')
-    //   //   $('.down-chevron').css('opacity', '1')
-    //   //   $('.initial-hide').css('display', 'initial')
-    //   //   // $('subheads p').text('Capabilities')
-    //   //
-    //   //   slideOut().then(() => {
-    //   //     setTimeout(() => {
-    //   //       $('.imgs img:nth-of-type(2)').css('display', 'none')
-    //   //       $('.imgs img:nth-of-type(2)').removeClass()
-    //   //     }, 1000)
-    //   //   })
-    //   //   landingCount++
-    //   //   $('.headers span').text('Capabilities\n')
-    //   //   $('.subheads p').text('Engage users instantly through an automated chat crafted with your brand’s voice, complete with quick replies based on common interactions.')
-    //   //   $('.landing-nav span:nth-of-type(' + landingCount + ')').addClass('active')
-    //   //   setTimeout(() => {
-    //   //     $('.imgs img:nth-of-type(' + landingCount + ')').css('display', 'initial')
-    //   //   }, 100)
-    //   //   setTimeout(() => {
-    //   //     $('.imgs img:nth-of-type(' + landingCount + ')').addClass('active')
-    //   //   }, 200)
-    //   //   // add active class to .imgs img:nth-of-type
-    //   //   // add active class to .subheads p:nth-of-type
-    //   } else {
-    //     $('.landing-nav span:nth-of-type(' + landingCount + ')').removeClass('active')
-    //     slideOut().then(() => {
-    //       setTimeout(() => {
-    //         $('.imgs img:last-of-type').css('display', 'none')
-    //         $('.imgs img:last-of-type').removeClass()
-    //       }, 1000)
-    //     })
-    //     landingCount = 1
-    //     $('.headers span').text('Meet Conversational e-Commerce')
-    //     $('.subheads p').text('Transform Messenger into an interactive, all-in-one e-commerce tool to maximize engagement, foster relationships and sell more.')
-    //     $('.landing-nav span:first-of-type').addClass('active')
-    //     setTimeout(() => {
-    //       $('.imgs img:first-of-type').css('display', 'initial')
-    //     }, 100)
-    //     setTimeout(() => {
-    //       $('.imgs img:first-of-type').addClass('active')
-    //     }, 200)
-    //     // add active class to .imgs img:first-of-type
-    //     // add active class to .subheads p:first-of-type
-    //   }
-    //   console.log(landingCount)
-    // }
-  })
+            $('.subheads').css('opacity', '0')
+            messengerIcon = false
+            console.log(messengerIcon)
+            $('.landing-nav span:last-of-type').addClass('active')
+            $('.landing-nav span:first-of-type').removeClass('active')
+            $('.down-chevron').css('opacity', '1')
+            $('.alert').css('left', '-77.5vw')
+            $('.imgs img.active').toggleClass('active')
+
+            setTimeout(() => {
+              $('.imgs img:first-of-type').css('display', 'none')
+            }, 500)
+
+            $('.imgs img:last-of-type').css('display', 'initial')
+            setTimeout(() => {
+              $('.imgs img:last-of-type').toggleClass('active')
+            }, 200)
+          } else {
+            messengerIcon = true
+            console.log('...add messenger icon')
+            $('.subheads').css('opacity', '1')
+            $('.landing-nav span:last-of-type').removeClass('active')
+            $('.landing-nav span:first-of-type').addClass('active')
+
+            $('.imgs img.active').toggleClass('active')
+            setTimeout(() => {
+              $('.imgs img:last-of-type').css('display', 'none')
+            }, 500)
+
+            $('.imgs img:first-of-type').css('display', 'initial')
+            setTimeout(() => {
+              $('.imgs img:first-of-type').toggleClass('active')
+            }, 200)
+          }
+
+      }
+
+      if (touchendY <= touchstartY) {
+          console.log('Swiped up');
+      }
+
+      if (touchendY >= touchstartY) {
+          console.log('Swiped down');
+      }
+
+      if (touchendY === touchstartY) {
+          console.log('Tap');
+      }
+  }
 })
